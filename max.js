83723475ab4b80973db7b746625293234f2f3384
Bùi Tuấn Anh <metwaroi31@gmail.com>
2019-03-29 23:20:57 +0700

    câu 9 10 lần 2

--- max.js
+++ max.js
@@ -1,12 +1,23 @@
 
-function max (Array) {
-    var length = Array.length
+function max () {
+    var lengthArguments = arguments.length
+    if (lengthArguments == 0 || lengthArguments > 1) {
+        return -1
+     }
+    for (var i = 0 ; i < lengthArguments;i++) {
+        if (!Array.isArray(arguments[i])) {
+            return -1
+        }
+    }
+    
+    Array = arguments[0]
+    var lengthArray = Array.length
     var currentElement
     if (length === 0) { 
         return 0 
     }
     var maxElement = Array[0]
-    for (var i = 1; i < length; i++) {
+    for (var i = 1; i < lengthArray; i++) {
         currentElement = Array[i]
         if (currentElement > maxElement) {
             maxElement = currentElement
@@ -14,5 +25,4 @@ function max (Array) {
     }
     return maxElement
 }
-
 module.export = max
--- maxInArray.js
+++ maxInArray.js
@@ -1,11 +1,22 @@
-function max (Array) {
-    var length = Array.length
+function max () {
+    var lengthArguments = arguments.length
+    if (lengthArguments == 0 || lengthArguments > 1) {
+        return -1
+     }
+    for (var i = 0 ; i < lengthArguments;i++) {
+        if (!Array.isArray(arguments[i])) {
+            return -1
+        }
+    }
+    
+    Array = arguments[0]
+    var lengthArray = Array.length
     var currentElement
     if (length === 0) { 
         return 0 
     }
     var maxElement = Array[0]
-    for (var i = 1; i < length; i++) {
+    for (var i = 1; i < lengthArray; i++) {
         currentElement = Array[i]
         if (currentElement > maxElement) {
             maxElement = currentElement
@@ -18,11 +29,19 @@ function max (Array) {
 
 
 function maxInArray() {
+
     var length = arguments.length
     var currentArray
     if (length === 0) { 
-        return 0 
+        return -1
     }
+
+    for (var i = 0 ; i < lengthArguments;i++) {
+        if (!Array.isArray(arguments[i])) {
+            return -1
+        }
+    }
+
     var maxCurrent = max(arguments[0])
     for (var i = 1; i < length;i++) {
         currentArray = arguments[i]
