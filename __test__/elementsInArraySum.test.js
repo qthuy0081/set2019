348c82594894fd3d42ff54c57aa604952043a7f7
Kiều Trí Đăng <kieutridang@gmail.com>
2019-03-29 23:00:42 +0700

    push array max test and elements in array sum test

--- __test__/elementsInArraySum.test.js
+++ __test__/elementsInArraySum.test.js
@@ -1,1 +1,24 @@
 // tổng các element trong các array
+const elementsInArraySum = require('../elementsInArraySum.js')
+const testArray = [1, 2, 3, 4, 5, 6]
+const testArrayWithUndefinedElement = [1, 2, 3, 4, 5, undefined]
+const emptyArray = []
+const nullArray = [null, null, null]
+
+describe('Test elementsInArraySum function', () => {
+  test('Normal case', () => {
+    expect(elementsInArraySum(testArray)).toBe(21)
+  })
+
+  test('Empty array', () => {
+    expect(elementsInArraySum(emptyArray)).toBe(null)
+  })
+
+  test('Null Array', () => {
+    expect(elementsInArraySum(nullArray)).toBe(null)
+  })
+
+  test('Array has undefined element', () => {
+    expect(elementsInArraySum(testArrayWithUndefinedElement)).toBe(15)
+  })
+})
