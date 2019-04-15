1bd35b92e5df9e42b267e3bbae5ec572f7c436ca
Võ Minh Trí <trivm17406c@st.uel.edu.vn>
2019-04-15 23:44:49 +0700

    finish_1
    finish_1

---
+++ male/script.js
@@ -0,0 +1,11 @@
+
+function show() {
+    var select = document.getElementById('select')
+    var choose = document.getElementById('option_1')
+    if(select = choose) {
+        alert('Still Be A Boy')
+    }
+    else if(select != choose) {
+        alert('Can Be In Jail')
+    }
+}
