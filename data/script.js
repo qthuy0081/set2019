53a666e172ff6326cf9cefad1668679b9bf30378
Nguyên <nguyenln2204@gmail.com>
2019-04-15 19:39:15 +0700

    DOM first version

---
+++ data/script.js
@@ -0,0 +1,10 @@
+function transit() {
+    debugger
+    var Gender = document.getElementsByName("gender").value;
+    if (Gender == undefined) {
+        alert("Anata no seibetsu?")
+    }
+    else {
+
+    }
+}
