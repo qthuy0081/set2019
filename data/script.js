4cdac6a0ec3af760c270f8a3428a3a20ebde75c6
Nguyên <nguyenln2204@gmail.com>
2019-04-16 16:46:32 +0700

    Nearly finished data form

--- data/script.js
+++ data/script.js
@@ -1,10 +1,15 @@
+var srcMale = "../male/index.html"
+var srcFemale = "../female/index.html"
+
 function transit() {
     debugger
-    var Gender = document.getElementsByName("gender").value;
-    if (Gender == undefined) {
-        alert("Anata no seibetsu?")
+//    window.open("test.html", "_self");
+    var gender = document.getElementsByName("gender");
+    if (gender[0].checked) {
+        window.open("../male/index.html")
     }
-    else {
-
-    }
+    else 
+        if (gender[1].checked) {
+            window.open("../female/index.html")
+        }
 }
