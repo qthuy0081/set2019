ce054290721d0410c6140358028717803be2635d
nam <cnam0303@gmail.com>
2019-04-16 22:27:36 +0700

    update

--- female/script.js
+++ female/script.js
@@ -1,12 +1,14 @@
 var Bdate = document.getElementById('myDate');
+var Hobby = document.getElementById('myHobbies');
 function validate() {
-    let flag =true
     if(Bdate.value == '') {
         Bdate.style.border = "2px solid red"
         document.getElementById('Bvalid').innerText = '*this field is madatory'
-        flag = false
     }
-    return flag
+    if( Hobby.selectedIndex == '0') {
+        Hobby.style.border = "2px solid red"
+        document.getElementById('Hvalid').innerText = '*this field is madatory'
+    }
 }
 
 function condition() {
@@ -16,7 +18,8 @@ function condition() {
     if('2019'- a > 18 &&  myid == '1'){
         alert('Alert can marry!!')
     }
-    else if('2019'- a <= 18 &&  myid == '1'||'2019'- a <= 18 &&  myid == '0'|| '2019'- a <= 18 &&  myid == '2' || '2019'- a <= 18 &&  myid == '3' || '2019'- a <= 18 &&  myid == '4' ){
+    else if('2019'- a <= 18 &&  myid == '1'|| '2019'- a <= 18 &&  myid == '2' || '2019'- a <= 18 &&  myid == '3' || '2019'- a <= 18 &&  myid == '4' 
+            || '2019'- a > 18 &&  myid == '2' || '2019'- a > 18 &&  myid == '3' || '2019'- a > 18 &&  myid == '4' ){
         alert('Still be a girl!!')
     }
     return false;
