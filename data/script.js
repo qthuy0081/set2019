5b8f0f97495ba93f323d58370b0aa9ce13cc6ae9
Nguyên <nguyenln2204@gmail.com>
2019-04-17 10:02:59 +0700

    Fix

--- data/script.js
+++ data/script.js
@@ -6,27 +6,9 @@ function transit() {
     if (gender[0].checked) {
         window.open(srcMale, '_self')
     }
-<<<<<<< HEAD
-}
-function changeForm()
-{
-    var check = document.getElementById('male').checked
-    if( check == true) {
-        window.open('../male/index.html','_self')
-    }
-    else {
-        window.open('../female/index.html','_self')
-    }
-=======
     else 
         if (gender[1].checked) {
             window.open(srcFemale, '_self')
         }
     return false
-}
-function changeForm()
-{
-    window.open('../female/index.html','_self')
->>>>>>> DOM_Homework_Team1
-    return false;
 }
