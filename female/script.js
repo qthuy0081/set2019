f3124ad0f514260d21c3ab87fc3b402c951f6504
Nguyên <nguyenln2204@gmail.com>
2019-04-17 19:38:41 +0700

    .

--- female/script.js
+++ female/script.js
@@ -1,26 +1,26 @@
-var Bdate = document.getElementById('myDate');
-var Hobby = document.getElementById('myHobbies');
+var bDate = document.getElementById('myDate');
+var hobby = document.getElementById('myHobbies');
 
 function validate() {
-    if(Bdate.value == '') {
+    if(bDate.value == '') {
         document.getElementById('birthValid').style.display = "block"
-        Bdate.style.border = "2px solid red"
+        bDate.style.border = "2px solid red"
         document.getElementById('birthValid').innerText = '*this field is madatory'
     }
 }
 
 function condition() {
-    if(Bdate.value!= '') {
-        var age = getAge(Bdate.value)
+    if(bDate.value!= '') {
+        var age = getAge(bDate.value)
         var myid = document.getElementById('myHobbies').selectedIndex 
         if( age > 18 &&  myid == '1') {
             document.getElementById('birthValid').style.display = "none"
-            Bdate.style.border = "1px solid gray"
+            bDate.style.border = "1px solid gray"
             alert('Alert can marry!!')
         }
         else if( age <= 18 ||  myid == '0') {
             document.getElementById('birthValid').style.display = "none"
-            Bdate.style.border = "1px solid gray"
+            bDate.style.border = "1px solid gray"
             alert('Still be a girl!!')
         }
     }
@@ -34,19 +34,19 @@ function Back() {
 function getAge(dateString) {
 
     var dates = dateString.split("-");
-    var d = new Date();
+    var curentDate = new Date();
 
     var userYear = dates[0];
     var userMonth = dates[1];
     var userDay = dates[2];
 
-    var curentDay = d.getDate();
-    var curentMonth = d.getMonth()+1;
-    var curentYear = d.getFullYear();
+    var curentDay = curentDate.getDate();
+    var curentMonth = curentDate.getMonth()+1;
+    var curentYear = curentDate.getFullYear();
 
     var age = curentYear - userYear;
 
-    if((curentMonth < userMonth) || ( (curentMonth == userMonth) && curentDay < userDay   )){
+    if((curentMonth < userMonth) || ( (curentMonth == userMonth) && curentDay < userDay)){
         age--;
     }
     return age;
