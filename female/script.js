eaefc4399748d76a675bf4e26e9c1f04c9cfe42f
nam <cnam0303@gmail.com>
2019-04-17 19:44:30 +0700

    update

--- female/script.js
+++ female/script.js
@@ -51,3 +51,17 @@ function getAge(dateString) {
     }
     return age;
 }
+//set max DoB
+var today = new Date();
+var dd = today.getDate();
+var mm = today.getMonth()+1;
+var yyyy = today.getFullYear();
+if(dd<10){
+       dd='0'+dd
+   }
+   if(mm<10){
+       mm='0'+mm
+   }
+
+today = yyyy+'-'+mm+'-'+dd;
+document.getElementById("myDate").setAttribute("max", today);
