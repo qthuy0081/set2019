b2f5af145104bfbbedde1d1a050427c8cf767e31
Nguyen Huu Quyen <huuquyen1412@gmail.com>
2019-04-17 12:41:35 +0700

    commit female form (fix bug)
    Bug fixed

--- female/script.js
+++ female/script.js
@@ -3,12 +3,9 @@ var Hobby = document.getElementById('myHobbies');
 
 function validate() {
     if(Bdate.value == '') {
+        document.getElementById('birthValid').style.display = "block"
         Bdate.style.border = "2px solid red"
         document.getElementById('birthValid').innerText = '*this field is madatory'
-    }
-    if( Hobby.selectedIndex == '0') {
-        Hobby.style.border = "2px solid red"
-        document.getElementById('hobbyValid').innerText = '*this field is madatory'
     }
 }
 
@@ -17,14 +14,19 @@ function condition() {
     var a = str[0]
     var myid = document.getElementById('myHobbies').selectedIndex 
     if('2019'- a > 18 &&  myid == '1'){
+        document.getElementById('birthValid').style.display = "none"
+        Bdate.style.border = "1px solid gray"
         alert('Alert can marry!!')
     }
     else if('2019'- a <= 18 &&  myid == '1'|| '2019'- a <= 18 &&  myid == '2' || '2019'- a <= 18 &&  myid == '3' || '2019'- a <= 18 &&  myid == '4'
             ||'2019'- a > 18 &&  myid == '2'|| '2019'- a > 18 &&  myid == '3' || '2019'- a > 18 &&  myid == '4' ){
+        document.getElementById('birthValid').style.display = "none"
+        Bdate.style.border = "1px solid gray"
         alert('Still be a girl!!')
     }
     return false;
 }
+
 function Back() {
     window.history.back()
 }
