4e1b8768694a35f2dddb192b029c1d9a3a406f17
Võ Minh Trí <trivm17406c@st.uel.edu.vn>
2019-04-17 17:05:50 +0700

    fix bug
    fix bug

--- male/script.js
+++ male/script.js
@@ -2,12 +2,9 @@ var Bdate = document.getElementById('myDate');
 var Hobby = document.getElementById('myHobbies');
 function validate() {
     if(Bdate.value == '') {
+        document.getElementById('birthValid').style.display='block'
         Bdate.style.border = "2px solid red"
         document.getElementById('Bvalid').innerText = '*this field is madatory'
-    }
-    if( Hobby.selectedIndex == '0') {
-        Hobby.style.border = "2px solid red"
-        document.getElementById('Hvalid').innerText = '*this field is madatory'
     }
 }
 
@@ -16,13 +13,14 @@ function condition() {
     var a = str[0]
     var myid = document.getElementById('myHobbies').selectedIndex 
     if('2019'- a > 18 &&  myid == '1'){
+        document.getElementById('birthValid').style.display='none'
+        Bdate.style.border = '1px solid gray'
         alert('Can be in Jail !!')
     }
-    else if('2019'- a <= 18 &&  myid == '1'
-            ||'2019'- a <= 18 &&  myid == '0'
-            || '2019'- a <= 18 &&  myid == '2' 
-            || '2019'- a <= 18 &&  myid == '3' 
-            || '2019'- a <= 18 &&  myid == '4' ){
+    else if('2019'- a <= 18 &&  myid == '1'|| '2019'- a <= 18 &&  myid == '2' || '2019'- a <= 18 &&  myid == '3' || '2019'- a <= 18 &&  myid == '4'
+    ||'2019'- a > 18 &&  myid == '2'|| '2019'- a > 18 &&  myid == '3' || '2019'- a > 18 &&  myid == '4'){
+            document.getElementById('birthValid').style.display='none'
+            Bdate.style.border = '1px solid gray'
         alert('Still be a boy !!')
     }
     return false;
