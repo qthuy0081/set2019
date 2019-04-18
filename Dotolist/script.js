61f46308411e0115dfaf9c3b6715ba14e6910c99
Bear Bui <bearq.bui@gmail.com>
2019-04-18 20:19:54 +0700

    Auto stash before rebase of "origin/ToDoList_Team1"

--- Dotolist/script.js
+++ Dotolist/script.js
@@ -14,3 +14,20 @@ function deleteFake(event) {
     item.innerHTML = item.innerHTML.replace('<button id="yes-button" onclick="deleteForever(event)">Yes</button>','')
     item.innerHTML += '<button id="delete-button" onclick="deleteItem(event)">Delete</button>'
 }
+function myFunction() {
+    document.getElementById("myDropdown").classList.toggle("show");
+}
+
+window.onclick = function(event) {
+  if (!event.target.matches('.dropbtn')) {
+    var dropdowns = document.getElementsByClassName("dropdown-content");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show');
+    
+      }
+    }
+  }
+}
