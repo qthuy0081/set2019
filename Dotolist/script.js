d41fced6a504e45d912f4de53964d592b6515e8c
nam <cnam0303@gmail.com>
2019-04-19 08:53:10 +0700

    update

--- Dotolist/script.js
+++ Dotolist/script.js
@@ -30,10 +30,21 @@ function deleteAttention() {
 }
 
 function deleteItem(event) {
+    if(document.getElementById('valid').style.display == 'block'||document.getElementById('valid-edit').style.display == 'block'){
+        document.getElementById('valid').style.display = 'none'
+        document.getElementById('valid-edit').style.display = 'none'
+    }
     var item = event.currentTarget.parentElement
-    event.currentTarget.remove()
-    item.innerHTML += '<button id="yes-button" onclick="deleteForever(event)">Yes</button>'
-    item.innerHTML += '<button id="no-button" onclick="deleteFake(event)">No</button>'
+    var a=item.childNodes[0]
+    var b=a.childNodes[0]
+    if(b.checked==true){
+       item.innerHTML = item.innerHTML.replace('<label><input type="checkbox">','<label><input type="checkbox" checked="true">')
+    }
+    item.innerHTML = item.innerHTML.replace('<button id="delete-button" onclick="deleteItem(event)">Delete</button>','')
+    item.innerHTML+='<button id="yes-button" onclick="deleteForever(event)">Yes</button>'
+    item.innerHTML+='<button id="no-button" onclick="deleteFake(event)">No</button>'
+        
+    
 }
 
 function deleteForever(event) {
@@ -79,7 +90,16 @@ function saveTask() {
         var saveIndex = document.getElementById('save')
         var taskList = document.getElementById('task-list')
         var liSave = taskList.childNodes[saveIndex.value]
-        liSave.innerHTML = '<label><input type="checkbox"/>'+checkValidate.value.trim()+'</label>'
+        var a=liSave.childNodes[0]
+        var b=a.childNodes[0]
+        if(b.checked==true)
+        {
+            liSave.innerHTML  = '<label><input type="checkbox" checked="true">'+checkValidate.value.trim()+'</label>' 
+        }
+        else
+        {
+            liSave.innerHTML  = '<label><input type="checkbox">'+checkValidate.value.trim()+'</label>'     
+        }
         liSave.innerHTML += '<button id="edit-button" onclick="editTaskName(event)">Edit</button>'
         liSave.innerHTML += '<button id="delete-button" onclick="deleteItem(event)">Delete</button>'
         document.getElementById('header').style.display = 'block'
@@ -105,17 +125,25 @@ function deleteAttentionEdit() {
 }
 
 function editTaskName(event) {
+    if(document.getElementById('header').style.display == 'none')
+    {
+        alert('Have to save before edit next')
+    }
+
+    else {
     var item = event.currentTarget.parentElement
     var item1 = event.currentTarget.parentElement.innerHTML
+    item1 = item1.replace('<label><input type="checkbox" checked="true">','')
     item1 = item1.replace('<label><input type="checkbox">','')
     item1 = item1.replace('</label>','')
     item1 = item1.replace('<button id="yes-button" onclick="deleteForever(event)">Yes</button>','')
     item1 = item1.replace('<button id="no-button" onclick="deleteFake(event)">No</button>','')
     item1 = item1.replace('<button id="edit-button" onclick="editTaskName(event)">Edit</button>','')
     item1 = item1.replace('<button id="delete-button" onclick="deleteItem(event)">Delete</button>','')
-    item.innerHTML+='save'
     var transit = document.getElementById('header-taskname-edit')
     transit.value = item1
+    var item2 = event.currentTarget
+    item2.innerHTML +='hello'
     document.getElementById('header').style.display = 'none'
     document.getElementById('save-edit').style.display = 'block'
     var taskList = document.getElementById('task-list')
@@ -126,7 +154,8 @@ function editTaskName(event) {
             break;
         }
     }
-    item.innerHTML = item.innerHTML.replace('save','')
+    item2.innerHTML = item2.innerHTML.replace('hello','')
     var saveIndex = document.getElementById('save')
     saveIndex.value=i
+    }
 }
