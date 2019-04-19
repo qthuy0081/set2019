2336909b95633da47c637e86a98186367f147c98
Bear Bui <bearq.bui@gmail.com>
2019-04-19 18:09:10 +0700

    Dropdown Text change Final

--- Dotolist/script.js
+++ Dotolist/script.js
@@ -76,7 +76,6 @@ function deleteForever(event) {
 
 function deleteFake(event) {
     var item = event.currentTarget.parentElement
-<<<<<<< HEAD
     event.currentTarget.remove()
     item.innerHTML = item.innerHTML.replace('<button class="yes-button" onclick="deleteForever(event)">Yes</button>','')
     item.innerHTML += '<button class="delete-button" onclick="deleteItem(event)">Delete</button>'
@@ -209,31 +208,10 @@ window.onclick = function(event) {
       var openDropdown = dropdowns[i];
       if(openDropdown.classList.contains('show')) {
         openDropdown.classList.remove('show');
-=======
-    var item1 = event.currentTarget.remove()
-    item.innerHTML = item.innerHTML.replace('<button id="yes-button" onclick="deleteForever(event)">Yes</button>','')
-    item.innerHTML += '<button id="delete-button" onclick="deleteItem(event)">Delete</button>'
-}
-//dropDown fnc
-function dropDown() {
-    document.getElementById("dropDownList").classList.toggle("show");
-}
-
-window.onclick = function(event) {
-  if (!event.target.matches('.dropbtn')) {
-    var dropdowns = document.getElementsByClassName("dropdown-content");
-    var i;
-    for (i = 0; i < dropdowns.length; i++) {
-      var openDropdown = dropdowns[i];
-      if (openDropdown.classList.contains('show')) {
-        openDropdown.classList.remove('show');
-    
->>>>>>> ToDoList_Team1_Dropdown
       }
     }
   }
 }
-<<<<<<< HEAD
 var buttonText = document.getElementsByClassName('drop-button')
 
 function allShow() {
@@ -259,6 +237,3 @@ function undoneShow() {
     doneList.style.display = 'none'
     buttonText[0].innerText = 'Undone'
 }
-=======
-//Dropdown fnc
->>>>>>> ToDoList_Team1_Dropdown
