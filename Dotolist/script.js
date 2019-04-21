ac65b7a4d326210071be8573bf107446c835aa72
nam <cnam0303@gmail.com>
2019-04-21 07:49:31 +0700

    scale
    scale

--- Dotolist/script.js
+++ Dotolist/script.js
@@ -1,4 +1,52 @@
 //add
+var maxHeightBodyList = document.getElementById('body').style.maxHeight
+var toDoList = document.getElementById('to-do-list')
+var instructionVideo = document.getElementById('instruction-video')
+var statistic = document.getElementById('statistic')
+function myFunction(x) {
+    if (x.matches) { // If media query matches
+        var width = window.innerWidth - 20
+        var height = (window.innerHeight - 60)/3
+        toDoList.style.width = width.toString() + 'px'
+        toDoList.style.height = height.toString() + 'px'
+        toDoList.style.cssFloat = 'left'
+        toDoList.style.margin = '0px 0px 20px 0px'
+        instructionVideo.style.width = width.toString() + 'px'
+        instructionVideo.style.height = height.toString() + 'px'
+        instructionVideo.style.margin = '0px 0px 20px 0px'
+        instructionVideo.style.cssFloat = 'left'
+        statistic.style.width = width.toString() + 'px'
+        statistic.style.height = height.toString() + 'px'
+        statistic.style.margin = '0px'
+        statistic.style.cssFloat = 'left'
+        var maxHeight = height - 110
+        maxHeightBodyList = maxHeight.toString() + 'px'
+    } 
+    else {
+        var width = (window.innerWidth - 30) / 2
+        var height = (window.innerHeight - 20)
+        var heightRight = height / 2 - 10
+        toDoList.style.width = width.toString() + 'px'
+        toDoList.style.height = height.toString() + 'px'
+        toDoList.style.cssFloat = 'left'
+        toDoList.style.margin = '0px 5px 0px 0px'
+        instructionVideo.style.width = width.toString() + 'px'
+        instructionVideo.style.height = heightRight.toString() + 'px'
+        instructionVideo.style.cssFloat = 'right'
+        instructionVideo.style.margin = '0px 0px 10px 5px'
+        statistic.style.width = width.toString() + 'px'
+        statistic.style.height = heightRight.toString() + 'px'
+        statistic.style.cssFloat = 'right'
+        statistic.style.margin = '10px 0px 0px 5px'
+        var maxHeight = height - 80
+        maxHeightBodyList = maxHeight.toString() + 'px'
+    }
+  }
+  
+var x = window.matchMedia("(max-width: 768px)")
+myFunction(x) // Call listener function at run time
+x.addListener(myFunction)
+
 function add() {
     var check = document.getElementById('header-taskname')
     if (check.value.trim() != '') {
@@ -30,48 +78,52 @@ function deleteAttention() {
 }
 
 function deleteItem(event) {
+    var item = event.currentTarget.parentElement
+    var nodeFirst = item.childNodes[0]
+    var nodeSecond = nodeFirst.childNodes[0]
     if (document.getElementById('valid').style.display == 'block'|| document.getElementById('valid-edit').style.display == 'block') {
         document.getElementById('valid').style.display = 'none'
         document.getElementById('valid-edit').style.display = 'none'
     }
-    var item = event.currentTarget.parentElement
-    var nodeFirst = item.childNodes[0]
-    var nodeSecond = nodeFirst.childNodes[0]
     if (nodeSecond.checked == true) {
        item.innerHTML = item.innerHTML.replace('<label><input type="checkbox onclick="disabledButton(event)">','<label><input type="checkbox" checked="true">')
     }
     item.innerHTML = item.innerHTML.replace('<button class="edit-button" onclick="editTaskName(event)">Edit</button>','')
     item.innerHTML = item.innerHTML.replace('<button class="delete-button" onclick="deleteItem(event)">Delete</button>','')
-    item.innerHTML+='<button class="no-button" onclick="deleteFake(event)">No</button>'
-    item.innerHTML+='<button class="yes-button" onclick="deleteForever(event)">Yes</button>'
+    item.innerHTML += '<button class="no-button" onclick="deleteFake(event)">No</button>'
+    item.innerHTML += '<button class="yes-button" onclick="deleteForever(event)">Yes</button>'
 }
 
 function deleteForever(event) {
-    var item=event.currentTarget.parentElement
+    var item = event.currentTarget.parentElement
     item.innerHTML += 'check'
-    var checkDisplay = document.getElementById('header')
-    if(checkDisplay.style.display == 'none') {
-        if(item.innerHTML == document.getElementById('task-list').childNodes[document.getElementById('save').value].innerHTML) {
-            checkDisplay.style.display = 'block'
-            document.getElementById('save-edit').style.display = 'none'
-            document.getElementById('header-taskname-edit').value=''
-        }
-        else {
-            item.innerHTML = item.innerHTML.replace('check','')
-            item.innerHTML += 'delete'
-            var taskList = document.getElementById('task-list')
-            for ( var i = 0 ; i<taskList.childNodes.length;i++) {
-                if(item.innerHTML == taskList.childNodes[i].innerHTML) {
-                    if(i < document.getElementById('save').value) {
-                        document.getElementById('save').value--
-                        break
-                    }
-                }
-            }
-        }
+    var headerDisplay = document.getElementById('header')
+    if(header.style.display == 'none') {
+        checkDisplay(headerDisplay,item)
     }
     item.remove()
-    var count = 0
+}
+function checkDisplay(objectDisplay, item) {
+    if (item.innerHTML == document.getElementById('task-list').childNodes[document.getElementById('save').value].innerHTML) {
+        objectDisplay.style.display = 'block'
+        document.getElementById('save-edit').style.display = 'none'
+        document.getElementById('header-taskname-edit').value=''
+    }
+    else {
+        item.innerHTML = item.innerHTML.replace('check','')
+        item.innerHTML += 'delete'
+        var taskList = document.getElementById('task-list')
+        checkSave(item,taskList)
+    }
+}
+function checkSave(item, taskList) {
+    var i = 0
+    while (item.innerHTML != taskList.childNodes[i].innerHTML) {
+        i++
+    }
+    if (i < document.getElementById('save').value) {
+        document.getElementById('save').value--
+    }
 }
 
 function deleteFake(event) {
@@ -85,23 +137,27 @@ function deleteFake(event) {
 //save
 function saveTask() {
     var checkValidate = document.getElementById('header-taskname-edit')
-    if(checkValidate.value.trim() != '') {
+    if (checkValidate.value.trim() != '') {
         var saveIndex = document.getElementById('save')
         var taskList = document.getElementById('task-list')
         var liSave = taskList.childNodes[saveIndex.value]
-        var nodeFirst = liSave.childNodes[0]
+        var nodeFirst = liSave.childNodes[0]     
         var nodeSecond = nodeFirst.childNodes[0]
-        if(nodeSecond.checked == true) {
-            liSave.innerHTML  = '<label><input type="checkbox" onclick="disabledButton(event)" checked="true">'+checkValidate.value.trim() + '</label>' 
-        }
-        else {
-            liSave.innerHTML  = '<label><input type="checkbox" onclick="disabledButton(event)">'+checkValidate.value.trim() + '</label>'     
-        }
+        checkNode(nodeSecond, liSave, checkValidate.value)
         liSave.innerHTML += '<button class="delete-button" onclick="deleteItem(event)">Delete</button>'
         liSave.innerHTML += '<button class="edit-button" onclick="editTaskName(event)">Edit</button>'
         document.getElementById('header').style.display = 'block'
         document.getElementById('save-edit').style.display = 'none'
         document.getElementById('valid-edit').style.display = 'none'
+    }
+}
+
+function checkNode(node, liSave, checkValidateValue) {
+    if (node.checked == true) {
+        liSave.innerHTML  = '<label><input type="checkbox" onclick="disabledButton(event)" checked="true">'+checkValidateValue.trim() + '</label>' 
+    }
+    else {
+        liSave.innerHTML  = '<label><input type="checkbox" onclick="disabledButton(event)">'+checkValidateValue.trim() + '</label>'     
     }
 }
 
@@ -125,34 +181,28 @@ function editTaskName(event) {
     if(document.getElementById('header').style.display == 'none') {
         alert('Have to save before edit next')
     }
-
     else {
         var item = event.currentTarget.parentElement
         var item1 = event.currentTarget.parentElement.innerHTML
-        item1 = item1.replace('<label><input type="checkbox" onclick="disabledButton(event)" checked="true">','')
-        item1 = item1.replace('<label><input type="checkbox" onclick="disabledButton(event)">','')
-        item1 = item1.replace('</label>','')
-        item1 = item1.replace('<button class="yes-button" onclick="deleteForever(event)">Yes</button>','')
-        item1 = item1.replace('<button class="no-button" onclick="deleteFake(event)">No</button>','')
-        item1 = item1.replace('<button class="edit-button" onclick="editTaskName(event)">Edit</button>','')
-        item1 = item1.replace('<button class="delete-button" onclick="deleteItem(event)">Delete</button>','')
         var transit = document.getElementById('header-taskname-edit')
-        transit.value = item1
+        var saveIndex = document.getElementById('save')
+        transit.value = item.childNodes[0].innerText
         var item2 = event.currentTarget
         item2.innerHTML += 'hello'
         document.getElementById('header').style.display = 'none'
         document.getElementById('save-edit').style.display = 'block'
-        var taskList = document.getElementById('task-list')
-        var i = 0
-        for(i; i < taskList.childNodes.length; i++) {
-            if(taskList.childNodes[i].innerHTML == item.innerHTML) {
-                break;
-            }
-        }
         item2.innerHTML = item2.innerHTML.replace('hello','')
-        var saveIndex = document.getElementById('save')
-        saveIndex.value = i
+        saveIndex.value = findIndex(item)
     }
+}
+
+function findIndex(item) {
+    var taskList = document.getElementById('task-list') 
+    var i = 0
+    while(taskList.childNodes[i].innerHTML != item.innerHTML) {
+        i++
+    }
+    return i
 }
 
 function disabledButton(event) {
@@ -162,55 +212,62 @@ function disabledButton(event) {
         checkParent.innerHTML = checkParent.innerHTML.replace('checked="true"','')
         checkParent.innerHTML += '<button class="delete-button" onclick="deleteItem(event)">Delete</button>'
         checkParent.innerHTML += '<button class="edit-button" onclick="editTaskName(event)">Edit</button>'
-        checkParent.innerHTML = checkParent.innerHTML.replace('<strike>','') 
-        checkParent.innerHTML = checkParent.innerHTML.replace('</strike>','') 
         var taskList = document.getElementById('task-list')
         var item = document.createElement('li')
         item.innerHTML = checkParent.innerHTML
+        item.childNodes[0].style.textDecoration = 'none'
         taskList.append(item)
         checkParent.remove()
-
     }
     else {
-        var onsave = document.getElementById('header')
-        if(onsave.style.display != 'none') {
+        checkSaveDisplay(checkParent, check)
+    }
+}
+
+function checkSaveDisplay(checkParent, check) {
+    var onsave = document.getElementById('header')
+    if(onsave.style.display == 'none') {
+        document.getElementById('valid-edit').style.display='block'
+        document.getElementById('valid-edit').innerText = 'Saving before changing'
+        check.childNodes[0].checked = false
+    }
+    else {
         checkParent.innerHTML = checkParent.innerHTML.replace('(event)"','(event)" checked="true"')
         checkParent.innerHTML = checkParent.innerHTML.replace('<button class="yes-button" onclick="deleteForever(event)">Yes</button>','')
         checkParent.innerHTML = checkParent.innerHTML.replace('<button class="no-button" onclick="deleteFake(event)">No</button>','')
         checkParent.innerHTML = checkParent.innerHTML.replace('<button class="edit-button" onclick="editTaskName(event)">Edit</button>','')
         checkParent.innerHTML = checkParent.innerHTML.replace('<button class="delete-button" onclick="deleteItem(event)">Delete</button>','')
-        var checkInner = checkParent.innerText
-        checkParent.innerHTML = checkParent.innerHTML.replace(checkInner,'<strike>'+checkInner+'</strike>')
         var doneList = document.getElementById('done-list')
         var item = document.createElement('li')
         item.innerHTML = checkParent.innerHTML
+        item.childNodes[0].style.textDecoration = 'line-through'
         doneList.append(item)
         checkParent.remove()
-        }
-        else {
-            var checkValidate = document.getElementById('header-taskname-edit')
-            document.getElementById('valid-edit').style.display='block'
-            document.getElementById('valid-edit').innerText = 'Saving before changing'
-            check.childNodes[0].checked = false
-        }
     }
 }
-
 function dropDown() {
     document.getElementById("dropdown-list").classList.toggle("show");
 }
 
 window.onclick = function(event) {
-  if (!event.target.matches('.drop-button')) {
+    if (!event.target.matches('.drop-button')) {
+        dropdownShow()
+    }
+}
+
+function dropdownShow() {
     var dropdowns = document.getElementsByClassName("dropdown-content");
     var i = 0;
     for (i; i < dropdowns.length; i++) {
-      var openDropdown = dropdowns[i];
-      if (openDropdown.classList.contains('show')) {
-        openDropdown.classList.remove('show');
-      }
+        var openDropdown = dropdowns[i];
+        openDropdownShow(openDropdown);
     }
-  }
+}
+
+function openDropdownShow(openDropdown) {
+    if (openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show');
+    }
 }
 var buttonText = document.getElementsByClassName('drop-button')
 
@@ -223,11 +280,19 @@ function allShow() {
 }
 
 function doneShow() {
+    var onsave = document.getElementById('header')
+    if (onsave.style.display == 'none') {
+        var checkValidate = document.getElementById('header-taskname-edit')
+        document.getElementById('valid-edit').style.display='block'
+        document.getElementById('valid-edit').innerText = 'Saving before changing'    
+    }
+    else {
     var listTask = document.getElementById('task-list')
     var doneList = document.getElementById('done-list')
     listTask.style.display = 'none'
     doneList.style.display = 'block'
     buttonText[0].innerText = 'Done'
+    }
 }
 
 function undoneShow() {
