5073e903479fbd2af7d659a7ed6139f6a150657a
Chau <tranhoaichau.00@gmail.com>
2019-04-21 22:20:58 +0700

    update video timeline

--- videoPlayPauseTimeLine/script.js
+++ videoPlayPauseTimeLine/script.js
@@ -2,6 +2,7 @@ var video = document.querySelector('.videoplayer')
 var progress = document.querySelector('.timeline-progress')
 var playOrPauseBtn = document.getElementById('play-pause')
 var volumeBtn = document.getElementById('mute-unmute')
+var timeline = document.getElementById('timeline')
 
 function playOrPause() {
     if(video.paused) {
@@ -18,9 +19,18 @@ video.addEventListener('timeupdate', function() {
     var timeposition = video.currentTime/video.duration;
     progress.style.width = timeposition * 100 + "%";
     if (video.ended) {
-        playOrPauseBtn.className = 'play'; 
+        playOrPauseBtn.innerHTML = '<i class="fas fa-play"></i>'
     }
 })
+
+video.addEventListener('click',function() {
+    playOrPause();
+})
+
+function timeChooser() {
+    var chosenTime = event.offsetX / timeline.offsetWidth * video.duration
+    video.currentTime = chosenTime
+}
 
 function muteOrUnmute() {
     if(video.volume == 1.0) {
