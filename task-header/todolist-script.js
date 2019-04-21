08502507325f3c960b74645f661e2c652ccb419b
Bùi Tuấn Anh <metwaroi31@gmail.com>
2019-04-21 22:47:05 +0700

    hai hàm getStatistic() ??

--- task-header/todolist-script.js
+++ task-header/todolist-script.js
@@ -220,7 +220,6 @@ function displayUndoneTasks() {
 
 
 function getStatistic() {
-  alert('run')
   let statisticSpace = document.getElementById('statistic-content')
   statisticSpace.innerHTML = ''
   let tasksCheckers = document.getElementsByClassName('input-task-checkbox')
@@ -242,25 +241,4 @@ function getStatistic() {
 
   statisticSpace.innerHTML += '<div>Done:</div>' + '<div>'+ doneRate +'</div>'
   statisticSpace.innerHTML += '<div>Undone:</div>' + '<div>'+ undoneRate +'</div>'
-}
-function getStatistic() {
-    let statisticSpace = document.getElementById('statistic-content')
-    statisticSpace.innerHTML = ''
-    let tasksCheckers = document.getElementsByClassName('input-task-checkbox')
-    let numberOfTasks = tasksCheckers.length
-    let doneRate = 0
-    let undoneRate = 0 
-    for (var i = 0; i < numberOfTasks;i++) {
-        if (tasksCheckers.value) {
-            doneRate = doneRate + 1
-        } else {
-            undoneRate = undoneRate + 1
-        }
-    }
-
-    doneRate = doneRate/numberOfTasks
-    undoneRate = undoneRate/numberOfTasks
-    statisticSpace.innerHTML += '<div>Done:</div>' + '<div>'+ doneRate +'</div>'
-    statisticSpace.innerHTML += '<div>Undone:</div>' + '<div>'+ undoneRate +'</div>'
-    
 }
