1b527df29ef464670a1c5ec9e16de388532b66cf
Vu Thien <St.violent@gmail.com>
2019-04-21 15:28:25 +0700

    finishes

--- task-header/todolist-script.js
+++ task-header/todolist-script.js
@@ -217,9 +217,8 @@ function displayUndoneTasks() {
       }
   }
 }
-function displayinStatistic(){
 
-}
+
 function getStatistic() {
   alert('run')
   let statisticSpace = document.getElementById('statistic-content')
@@ -243,4 +242,25 @@ function getStatistic() {
 
   statisticSpace.innerHTML += '<div>Done:</div>' + '<div>'+ doneRate +'</div>'
   statisticSpace.innerHTML += '<div>Undone:</div>' + '<div>'+ undoneRate +'</div>'
+}
+function getStatistic() {
+    let statisticSpace = document.getElementById('statistic-content')
+    statisticSpace.innerHTML = ''
+    let tasksCheckers = document.getElementsByClassName('input-task-checkbox')
+    let numberOfTasks = tasksCheckers.length
+    let doneRate = 0
+    let undoneRate = 0 
+    for (var i = 0; i < numberOfTasks;i++) {
+        if (tasksCheckers.value) {
+            doneRate = doneRate + 1
+        } else {
+            undoneRate = undoneRate + 1
+        }
+    }
+
+    doneRate = doneRate/numberOfTasks
+    undoneRate = undoneRate/numberOfTasks
+    statisticSpace.innerHTML += '<div>Done:</div>' + '<div>'+ doneRate +'</div>'
+    statisticSpace.innerHTML += '<div>Undone:</div>' + '<div>'+ undoneRate +'</div>'
+    
 }
