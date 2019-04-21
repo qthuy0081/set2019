4c01f91ae3541dc222e2ae75db89a7e97c533dac
Nguyễn Duy Hiếu <hidxhidxhid@gmail.com>
2019-04-21 14:57:43 +0700

    update

--- task-header/todolist-script.js
+++ task-header/todolist-script.js
@@ -218,3 +218,117 @@ function displayUndoneTasks() {
       }
   }
 }
+
+//video
+var videoPlayer = document.getElementById('video-player')
+var videoScreen = document.getElementById('video-screen')
+var videoPanel = document.getElementById('video-panel')
+var videoTime = document.getElementById('video-time')
+var watched = 0
+var timer
+
+videoScreen.muted = true // for auto-play
+
+function convertToMinute (length) {
+  let minute = Math.floor(length / 60)
+  let second = Math.floor(length % 60)
+  if(second < 10)
+    second = '0' + second
+  return minute + ":" + second
+}
+
+function playVideo () {
+  videoScreen.play()
+  startProgressBar()
+  renderButton({
+    function: pauseVideo,
+    class: 'video-button',
+    id: 'pause-button',
+    icon: '<img src="https://img.icons8.com/ios/50/000000/pause-filled.png">',
+    replace: 'play-button'
+  })
+  videoTime.innerText = convertToMinute(videoScreen.duration)  
+}
+
+function pauseVideo () {
+  videoScreen.pause()
+  stopProgressBar()
+  renderButton({
+    function: playVideo,
+    class: 'video-button',
+    id: 'play-button',
+    icon: '<img src="https://img.icons8.com/metro/26/000000/play.png">',
+    replace: 'pause-button'
+  })
+}
+
+function renderButton (options) {
+  let newButton = document.createElement('button')
+  newButton.addEventListener('click', options.function)
+  newButton.className = options.class
+  newButton.id = options.id
+  newButton.innerHTML = options.icon
+  videoPanel.replaceChild(newButton, document.getElementById(options.replace))
+}
+
+function startProgressBar () {
+  let watchedBar = document.getElementById('watched-bar')
+  let length = videoScreen.duration
+  let timeWatched = document.getElementById('time-watched')
+  
+  timer = setInterval(frame, 100)
+  function frame () {
+    if (watched >= length) {
+      renderButton({
+        function: playVideo,
+        class: 'video-button',
+        id: 'play-button',
+        icon: '<img src="https://img.icons8.com/metro/26/000000/play.png">',
+        replace: 'pause-button'
+      })
+      watched = 0
+      stopProgressBar()
+    } 
+    else {
+      watched = watched + 0.1;
+      timeWatched.innerText = convertToMinute(watched)
+      watchedBar.style.width = watched / length * 100 + '%';
+    }
+  }
+}
+
+function stopProgressBar () {
+  clearInterval(timer)
+}
+
+function changeVolume () {
+  if(!videoScreen.muted) {
+    videoScreen.muted = true
+    renderButton({
+      function: changeVolume,
+      class: 'video-button',
+      id: 'mute-button',
+      icon: '<img src="https://img.icons8.com/metro/26/000000/no-audio.png">',
+      replace: 'unmute-button'
+    })
+  }
+  else {
+    videoScreen.muted = false
+    renderButton({
+      function: changeVolume,
+      class: 'video-button',
+      id: 'unmute-button',
+      icon: '<img src="https://img.icons8.com/metro/26/000000/high-volume.png">',
+      replace: 'mute-button'
+    })
+  }
+}
+
+function toggleFullScreen () {
+  if (videoScreen.mozRequestFullScreen) {
+    videoScreen.mozRequestFullScreen()
+  } 
+  else if (videoScreen.webkitRequestFullScreen) {
+    videoScreen.webkitRequestFullScreen()
+  }  
+}
